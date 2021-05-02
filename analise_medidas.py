35d09502993ea2496d0862959fad329638e11295
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2021-05-02 13:22:50 -0300

    :sparkles: add: formatação de data e hora

--- analise_medidas.py
+++ analise_medidas.py
@@ -1,12 +1,14 @@
 #%% Importando Bibliotecas
 import pandas as pd
+import numpy
+from datetime import datetime
 import psycopg2 as pg
 import matplotlib.pyplot as plt
 
 #%% Conexão com o banco de dados
 DB_HOST = "localhost"
-DB_USER = "username"
-DB_PASS = "password"
+DB_USER = "postgres"
+DB_PASS = "teste123"
 DB_NAME = "medidor"
 
 TB_NAME = "medida"
@@ -18,24 +20,34 @@ cur = conn.cursor()
 df = pd.read_sql_query("SELECT * FROM {}".format(TB_NAME), con=conn)
 df.info()
 
-# %% Analise Exploratória
+#%% Analise Exploratória
 # Primeiros dados
 print(df.head())
 
-# %% Ultimos dados
+#%% Ultimos dados
 print(df.tail())
 
 #%% Criando colunas com dados de data_reg separados
 df['ano']= df['data_reg'].dt.year
 df['mes']= df['data_reg'].dt.month
 df['dia']= df['data_reg'].dt.day
-df['hora']= df['data_reg'].dt.time
+
+#df['hora']= df['data_reg'].dt.time
 
 print(df.head())
 
-#%%
-fig, ax = plt.subplots()
-ax.plot(df["dia"], df["pulsacao"])
-
-#%%
-plt.plot(df["dia"], df["peso"])
+#%% formatando a data e hora
+df['data'] = df.data_reg.apply(lambda x: x.strftime('%d/%m/%Y')) 
+df['hora'] = df.data_reg.apply(lambda x: x.strftime('%H:%M'))
+# %%
+print(df)
+# %% Obtendo dia da semana e periodo da hora se é AM ou PM
+df['dia_semana'] = df.data_reg.apply(lambda x: x.strftime('%A')) 
+df['periodo_hora'] = df.data_reg.apply(lambda x: x.strftime('%p'))
+# %%
+print(df)
+# %%
+df.peso.plot()
+# %%
+df.peso.hist()
+# %%
